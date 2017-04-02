9e2e2b3cc97e3f1bc6fd374c9cdf173f4b623d51
Arun Tej <aruntej@outlook.com>
2017-04-01 23:08:15 -0700

    Added setup and teardown

--- src/main/java/com/arun/test2/C1.java
+++ src/main/java/com/arun/test2/C1.java
@@ -8,6 +8,12 @@ public class C1 {
         // call a1 methods
     }
     
- 
+    public void setup( A1 a1 ) {
+        // call a1 methods
+    }
+    
+    public void teardown( A1 a1 ) {
+        // call a1 methods
+    }
 }
  
