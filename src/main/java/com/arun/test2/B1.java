9b52593e1de02458ae6bafb659b468307392782c
Arun Tej <aruntej@outlook.com>
2017-04-01 22:54:43 -0700

    Work in progress Test case 2

--- src/main/java/com/arun/test2/B1.java
+++ src/main/java/com/arun/test2/B1.java
@@ -2,6 +2,10 @@ package com.arun.test2;
 
   
 
+/**
+ * @author chennadi
+ * class for test case 2
+ */
 public class B1 extends P implements A1 {
    public static void main(String[] args){
 	   B1 b = new B1();
