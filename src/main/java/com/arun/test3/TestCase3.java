5480e8164a8aa938732be2b54b6b7d7ee2ca1b49
Arun Tej <aruntej@outlook.com>
2017-04-13 14:58:41 -0700

    Added variables and toString method

--- src/main/java/com/arun/test3/TestCase3.java
+++ src/main/java/com/arun/test3/TestCase3.java
@@ -2,10 +2,31 @@ package com.arun.test3;
 
 public class TestCase3 {
 
+	int x;
+	int y;
+	int z;
+
+
 	public static void main(String[] args) {
 
 	}
 
 	
+	
+	@Override
+	public String toString() {
+		return "TestCase3 [x=" + x + ", y=" + y + ", z=" + z + "]";
+	}
+
+	
+
+	public TestCase3(int x, int y, int z) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+
 	public TestCase3(){}
 }
